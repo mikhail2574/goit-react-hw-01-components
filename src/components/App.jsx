79dda2b357c39672f8d4a502e9450e8f6e7d0a8e
mykhailo_naumenko <74735041+mikhail2574@users.noreply.github.com>
2023-08-30 19:00:31 +0200

    add project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
-export const App = () => {
+import user from 'user';
+import data from 'data';
+import { Profile } from 'components/profile';
+import { Fragment } from 'react';
+import { Statistics } from './statistics';
+
+export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+    <Fragment>
+    <Profile
+      name={user.username}
+      location={user.location}
+      tag={user.tag}
+      avatar={user.avatar}
+      followers={user.stats.followers}
+      views={user.stats.views}
+      likes={user.stats.likes}
+      />
+      <Statistics title="upload stats" stats={data} />
+      </Fragment>);
+}
---
+++ src/components/profile.jsx
@@ -0,0 +1,32 @@
+import "profile.modal.css";
+import PropTypes from "prop-types";
+
+export const Profile = ({ name, location, tag, avatar, followers, views, likes }) => {
+  return (
+      <div className="card">
+          <div className="upper-container">
+              <img className="avatar" src={avatar} alt="your icon" />
+              <h3 className="upper-name">{name}</h3>
+              <p className="upper-item">@{tag}</p>
+              <p className="upper-item">{location}</p>
+          </div>
+          <div className="lower-container">
+            <ul className="list">
+              <li className="lower-item">Followers <b>{followers}</b></li>
+              <li className="lower-item">Views <b>{views}</b></li>
+              <li className="lower-item">Likes <b>{likes}</b></li>
+            </ul>   
+          </div>
+      </div>
+  );
+};
+
+Profile.propTypes = {
+    avatar: PropTypes.string,
+    name: PropTypes.string,
+    location: PropTypes.string,
+    tag: PropTypes.string,
+    followers: PropTypes.number,
+    views: PropTypes.number,
+    likes: PropTypes.number,
+}
